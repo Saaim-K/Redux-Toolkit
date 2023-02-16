b2cb72ec2b9e35863d9429da244928671b3b5d7f
Saaim-K <saaimkhan221@gmail.com>
2023-02-16 21:46:05 +0500

    commit

--- src/Components/Store/Slices/UserSlice.jsx
+++ src/Components/Store/Slices/UserSlice.jsx
@@ -9,4 +9,5 @@ const usersSlice = createSlice({
       clearAllUsers: (state, action) => { }
    }
 });
+
 export default usersSlice
--- src/Components/Store/index.jsx
+++ src/Components/Store/index.jsx
@@ -1,1 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import usersSlice from "./Slices/UserSlice.jsx";
+
+const store = configureStore({
+   reducer: {}
+});
